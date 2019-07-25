d331ac1eef44ffe321a3b11630d694ee3f5270e1
Gabriel <pedromurillo96@gmail.com>
2019-07-24 17:36:34 -0700

    minor updates to all files

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+var correct = 0;
+var incorrect = 0;
+var unanswered = 0;
+
+
+if (document.getElementById("num1").checked === "female") {
+    score++
+
+}
+
+
+
+//$("h4").hide();
+//$("input").hide();
