0fd9e2325df4c376a4ab8de2de7c5e967a4fa456
Gabriel <pedromurillo96@gmail.com>
2019-07-25 01:29:42 -0700

    major changes to js file and minor changes to html file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,6 +3,14 @@
 // var unanswered = 0;
 
 
+// $("#second").hide();
+
+// $("#start").click(function(){
+//     $("#second").show();
+//     $("#first").css({"height": "auto"});
+//     $("#start").hide();
+// })
+
 
 function check(){
     var question1 = document.form1.question1.value;
@@ -17,49 +25,112 @@ function check(){
     var incorrect = 0;
     var unanswered = 0;
 
-    
+    //question 1 conditions
 	if (question1 == "Lord Boros") {
-		correct++;
-}
+        correct++;
+        
+    }
+
+    if (question1 != "Lord Boros"){
+        incorrect++;
+    }
+
+    if (question1 == ""){
+        unanswered++;
+    }
+    //question 2 conditions
 	if (question2 == "Erza") {
 		correct++;
-}	
+    }
+
+    if (question2 != "Erza"){
+        incorrect++;
+    }
+    
+    if (question2 == ""){
+        unanswered++;
+    }
+    //question 3 conditions
 	if (question3 == "Accelerator") {
 		correct++;
     }
+
+    if (question3 != "Accelerator"){
+        incorrect++;
+    }
+    
+    if (question3 == ""){
+        unanswered++;
+    }
+    //question 4 conditions
     if (question4 == "Madoka") {
 		correct++;
     }
+
+    if (question4 != "Madoka"){
+        incorrect++;
+    }
+    
+    if (question4 == ""){
+        unanswered++;
+    }
+    //question 5 conditions
     if (question5 == "Goblin") {
 		correct++;
     }
+
+    if (question5 != "Goblin"){
+        incorrect++;
+    }
+    
+    if (question5 == ""){
+        unanswered++;
+    }
+    //question 6 conditions
     if (question6 == "Erina") {
 		correct++;
     }
+
+    if (question6 != "Erina"){
+        incorrect++;
+    }
+    
+    if (question6 == ""){
+        unanswered++;
+    }
+    //question 7 conditions
     if (question7 == "Ais") {
 		correct++;
     }
+
+    if (question7 != "Ais"){
+        incorrect++;
+    }
+    
+    if (question7 == ""){
+        unanswered++;
+    }
+    //question 8 conditions
     if (question8 == "Father") {
 		correct++;
     }
-    console.log(correct);
+
+    if (question8 != "Father"){
+        incorrect++;
+    }
+    
+    if (question8 == ""){
+        unanswered++;
+    }
+
+    console.log(correct, incorrect, unanswered);
 }
 
-// $("#second").hide();
-
-// $("#start").click(function(){
-//     $("#second").show();
-//     $("#first").css({"height": "auto"});
-//     $("#start").hide();
-// })
 
 
 
 
-// if (document.getElementById("num1").checked === "female") {
-//     score++
 
-// }
 
 
 
